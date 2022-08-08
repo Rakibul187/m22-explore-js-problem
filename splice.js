453ba7756844ce0dd26f3c5095d6f2f458e4d3d3
Rakibul Islam <rakibul241221@gmail.com>
2022-08-08 11:51:16 +0600

    added splice

--- splice.js
+++ splice.js
@@ -1,4 +1,10 @@
-const friends = [12, 45, 32, 22, 44, 62, 29, 69, 87];
-const partial = friends.splice(2, 5);
-console.log(partial)
-console.log(friends)
+// const friends = [12, 45, 32, 22, 44, 62, 29, 69, 87];
+// const partial = friends.splice(2, 5);
+// console.log(partial)
+// console.log(friends)
+
+
+const friends2 = [11, 22, 33, 44, 55, 66, 77, 88];
+const partial2 = friends2.splice(2, 4, 20000, 300000, 40000, 500000, 300000, 40000, 550000);
+console.log(partial2)
+console.log(friends2)
